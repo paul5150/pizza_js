3d1b5c299c443b9919bbd272f85ae27e35745964
Guest User <student@epicodus.com>
2015-07-02 11:38:17 -0700

    work the way it's suppose to. Yay.

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,12 @@ function Pizza(cost){
   this.cost = 10;
 }
 
-Pizza.prototype.quantity = function(amount) {
-  this.cost *= amount;
-};
 
 Pizza.prototype.size = function(size) {
   if(size === "large") {
-    this.cost += 4;
-  } else if(size === "medium") {
     this.cost += 2;
+  } else if(size === "medium") {
+    this.cost += 1;
   } else if(size === "small"){
     this.cost;
   }
@@ -27,3 +24,33 @@ Pizza.prototype.toppings = function(toppings) {
     this.cost += 0;
   }
 }
+
+Pizza.prototype.quantity = function(amount) {
+  this.cost *= amount;
+};
+
+
+$(document).ready(function(){
+  var newPizza = new Pizza();
+  newPizza.cost = 0;
+
+  $("form#order").submit(function(event){
+
+    newPizza.cost = 10;
+    var inputtedQuantity = parseInt($("input#quantity").val());
+    var inputtedSize = $("select#size").val();
+    var inputtedToppings = $("select#toppings").val();
+
+    // newPizza.quantity(inputtedQuantity)
+    // newPizza.size(inputtedSize)
+    // newPizza.toppings(inputtedToppings)
+
+    (newPizza.cost + newPizza.size(inputtedSize) + newPizza.toppings(inputtedToppings)) * newPizza.quantity(inputtedQuantity)
+
+    $(".ordertotal").text(newPizza.cost);
+
+    event.preventDefault();
+
+    $("input#quantity").val("");
+  });
+});
