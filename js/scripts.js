401e6b40949d60f2949d4a00b819ca1639d0c0c0
Guest User <student@epicodus.com>
2015-07-02 10:12:14 -0700

    pass all specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,29 @@
 function Pizza(cost){
   this.cost = 10;
 }
+
+Pizza.prototype.quantity = function(amount) {
+  this.cost *= amount;
+};
+
+Pizza.prototype.size = function(size) {
+  if(size === "large") {
+    this.cost += 4;
+  } else if(size === "medium") {
+    this.cost += 2;
+  } else if(size === "small"){
+    this.cost;
+  }
+}
+
+Pizza.prototype.toppings = function(toppings) {
+  if(toppings === "Meat") {
+    this.cost += 4;
+  } else if(toppings === "Vegan"){
+    this.cost += 2;
+  } else if(toppings === "Veggie"){
+    this.cost += 1;
+  } else if(toppings === "Cheese") {
+    this.cost += 0;
+  }
+}
--- spec/specs.js
+++ spec/specs.js
@@ -7,5 +7,28 @@ describe("Pizza", function(){
     });
   });
 
+  describe("quantity", function(){
+    it("will return a base price based on quantity", function(){
+      var testPizza = new Pizza()
+      testPizza.quantity(2)
+      expect(testPizza.cost).to.equal(20)
+    });
+  });
+
+  describe("size", function() {
+    it("will return a price based on size", function() {
+      var testPizza = new Pizza()
+      testPizza.size("small")
+      expect(testPizza.cost).to.equal(10)
+    });
+  });
+
+  describe("toppings", function(){
+    it("will return a price based on toppings", function(){
+      var testPizza = new Pizza()
+      testPizza.toppings("Meat")
+      expect(testPizza.cost).to.equal(14)
+    });
+  });
 
 });
